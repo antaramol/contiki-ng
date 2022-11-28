ee4afa11aa1021326a67b69301eb14bad005116d
antaramol <antaramol@alum.us.es>
2022-11-28 21:36:48 +0100

    Cambios trabajo

--- practicas/P3/Red1/udp-server.c
+++ practicas/P3/Red1/udp-server.c
@@ -54,12 +54,12 @@ udp_rx_callback(struct simple_udp_connection *c,
          const uint8_t *data,
          uint16_t datalen)
 {
-  LOG_INFO("Received request '%.*s' from ", datalen, (char *) data);
+  printf("Received request '%.*s' from ", datalen, (char *) data);
   LOG_INFO_6ADDR(sender_addr);
-  LOG_INFO_("\n");
+  printf("\n");
 #if WITH_SERVER_REPLY
   /* send back the same string to the client as an echo reply */
-  LOG_INFO("Sending response.\n");
+  printf("Sending response.\n");
   simple_udp_sendto(&udp_conn, data, datalen, sender_addr);
 #endif /* WITH_SERVER_REPLY */
 }
